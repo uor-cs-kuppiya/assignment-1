ce52881bb5963d70df38b1342f77273b4f3c60f3
Yasith Buddhima <yasithbuddhima@gmail.com>
2025-04-10 09:18:44 +0530

    Initial Commit

---
+++ hello-world.c
@@ -0,0 +1,17 @@
+/*
+ * Assignment: Hello World in C
+ * Instructions:
+ * 1. Open this file in your preferred C IDE or text editor.
+ * 2. Complete the program to print "Hello, World!" to the console.
+ * 3. Compile the program using a C compiler (e.g., gcc).
+ * 4. Run the compiled program to verify the output.
+ * 5. Submit the completed code along with a screenshot of the output.
+ */
+
+#include <stdio.h>
+
+int main() {
+    // TODO: Write code to print "Hello, World!" to the console.
+    
+    return 0;
+}
